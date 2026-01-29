bf5cf91a1627a6ac1a0e35c663794e46b67ed128
Aryan <aryansinghtomar4@gmail.com>
2026-01-29 08:35:25 +0530

    Compleated copy constructor

---
+++ pratice/heapNumber.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+class HeapNumber {
+    int* p;
+public:
+    HeapNumber(int v) : p{new int(v)}{
+        std::cout << "Created" << std::endl;
+    }
+    HeapNumber(const HeapNumber& others) : p{new int(*others.p)}
+    {
+        std::cout << "created" << std::endl;
+    }
+    ~HeapNumber() {
+        delete p;
+        std::cout << "destroyed" << std::endl;
+    }
+};
+int main() {
+    HeapNumber a(10);
+    HeapNumber b = a;
+}
