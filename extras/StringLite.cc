f12e78f10018008775d83bbac952962326cfd867
Aryan <aryansinghtomar4@gmail.com>
2026-01-30 12:47:04 +0530

    finshed rule of five

--- extras/StringLite.cc
+++ extras/StringLite.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
+using std::cout;
+using std::endl;
 
 class StringLite
 {
@@ -12,12 +15,14 @@ public:
     {
         data = new char[length + 1]; //allocating new char of length(length + 1) to pointer data
         std::strcpy(data, str);
+        cout << "conturected" << endl;
     }
 
     StringLite(const StringLite &other) : length(other.length) //checking the length of current string
     {
         data = new char[length + 1]; //allocating new char of lenth(length + 1) 
         std::strcpy(data, other.data); // copying the data of previous string to current string
+        cout << "copy" << endl;
     }
     StringLite &operator=(const StringLite &other)
     {
@@ -28,12 +33,16 @@ public:
         length = other.length; // then just store value at different same as previous at different location
         data = new char[length + 1];
         std::strcpy(data, other.data);
+
+        cout << "copy assigined" << endl;
         return *this;
+        
     }
     StringLite(StringLite&& other) noexcept : data{other.data}, length{other.length}
     {
         other.data = nullptr;
         other.length = 0;
+        cout << "move" << endl;
     }
 
     StringLite& operator=(StringLite&& other) noexcept{
@@ -45,23 +54,32 @@ public:
         length=other.length;
 
         other.data=nullptr;
-        length=0;
+        other.length=0;
         
+        cout << "move assigined" << endl;
         return *this;
-
     }
 
     ~StringLite()
     {
         delete[] data; //freeing the memory of heap finally
+        cout << "distructed" << endl;
     }
 };
 
 int main()
 {
-    StringLite a("hello");
-    StringLite b = a;//copy constructor
-    StringLite c("world");
-    c = a;//copy assiginment
-    c = std::move (b);
+    std::vector<StringLite> v;
+    // StringLite a("hello");
+    // StringLite b = a;//copy constructor
+    // StringLite c("world");
+    // c = a;//copy assiginment
+    // c = std::move (b);
+
+    //vector
+// first it is constructing  moving then distrucing
+
+    v.push_back(StringLite("one"));// moving one time
+    v.push_back(StringLite("two"));// moving two times
+    v.push_back(StringLite("three")); // moving three times
 }
