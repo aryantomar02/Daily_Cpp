7fd0a5ee2d6dbd0a6ce4231c98b59b5fe301b231
Aryan <aryansinghtomar4@gmail.com>
2026-01-30 13:05:25 +0530

    added postfix to the file

--- src/Overloding/OverlodingPrefix.cc
+++ src/Overloding/OverlodingPrefix.cc
@@ -14,16 +14,24 @@ public:
     {
         ++x;
         ++d;
-        std::cout << "++Operator invoked" << std::endl;
+        std::cout << "prefix operator ++ invoked." << std::endl;
 
         return *this;
+    }
+
+    MyClass operator++(int)
+    {
+        MyClass temp(*this); //defult copy
+        operator++();
+        std::cout << "Postfix operator ++ invoked." << std::endl;
+        return temp;
     }
     
 };
 
 int main() {
     MyClass obj;
-    ++obj;
-    obj.operator++();
+    obj++;
+    obj.operator++(0);
 }
 
