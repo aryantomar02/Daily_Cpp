9ad49a15a25ff59c3b8651b4889daba6ceddd27e
Aryan <aryansinghtomar4@gmail.com>
2026-02-12 18:30:13 +0530

    Updated templates

--- src/templates/templates.cc
+++ src/templates/templates.cc
@@ -7,6 +7,7 @@ class intonly
 {
 private:
     T x;
+
 public:
     intonly(T xx);
     T myfunc(T argu);
@@ -14,24 +15,29 @@ public:
 template <typename T>
 intonly<T>::intonly(T xx) : x{xx}
 {
-    cout << "Const invoked, value passed is: "<< xx << endl;
+    cout << "Const invoked, value passed is: " << xx << endl;
 }
 
 template <typename T>
-T intonly<T>::myfunc(T xx) {
+T intonly<T>::myfunc(T xx)
+{
     return -1;
 }
 
-template<>
-int intonly<int>::myfunc(int xx) {
+template <>
+int intonly<int>::myfunc(int xx)
+{
     cout << "The int is: " << xx << endl;
     return xx;
 }
 
-int main() {
+int main()
+{
     intonly<int> o{2};
-    intonly<double> o2{3.1};
+    intonly<double> o2{3.0}; // now output would be 3 because compilor prints 3.0 as 3
+    intonly<double> o3{3.8}; // but this will be normal output in floating point numbers
 
     o.myfunc(10);
     o2.myfunc(10.0);
+    o3.myfunc(10.0);
 }
